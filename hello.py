545ba81dcce413f01180e9dee1bbf1280c46c79a
Pramit Ray <pramitray28@gmail.com>
2024-09-24 18:36:30 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,3 +8,10 @@ def hello_world():
 print('The sum of two numbers is':1+2)
 
 print('Hi my name is Pramit')
+
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+def product(x, y):
+  return x*y
+product(x, y)
